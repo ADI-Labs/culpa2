57fc9687ef74d96a8b62ec1c0dd7652504243a8d
Justin Woodbridge <me@justinwoodbridge.com>
2017-03-31 15:35:40 -0400

    aoeuaoeu

--- Culpa.py
+++ Culpa.py
@@ -22,13 +22,37 @@ def store_review():
 def get_professor():
     prof = request.args.get('prof')
     prof_searches = session.query(Professors.professor).filter(Professors.professor.last_name.like("%Rob%")).all()
-    # Return stub response for professor search.
-    with open("./data/professor.json", 'r') as f:
-        _data = f.read()
-        return_prof = json.loads(_data)
-        # for i in range(len(prof_searches)):
-        #     return_prof['messagess'][0]['quick_replies'][i]['title'] = prof_searches[i].first_name + " " + prof_searches[i].middle_name + " " + prof_searches[i].last_name
-        return jsonify(return_prof)
+
+    professor_options = []
+
+    for prof in prof_searches:
+        professor_options.append({
+            "title": prof.first_name + prof.last_name,
+            "set_attributes": {
+                "review_professor": prof.first_name + " " + prof.last_name,
+                "review_professor_id": 0
+            },
+            "block_names": ["Entry Class Demo"],
+            "type": "show_block"
+        })
+
+    response = {
+        'message': {
+            "text": "Which of these names looks right?",
+            "quick_replies": professor_options
+        }
+    }
+
+    return jsonify(response)
+
+    # return jsonify(json_profs)
+
+    # with open("./data/professor.json", 'r') as f:
+    #     _data = f.read()
+    #     return_prof = json.loads(_data)
+    #     # for i in range(len(prof_searches)):
+    #     #     return_prof['messagess'][0]['quick_replies'][i]['title'] = prof_searches[i].first_name + " " + prof_searches[i].middle_name + " " + prof_searches[i].last_name
+    #     return jsonify(return_prof)
 
 @app.route('/getClass')
 def get_class():
--- Models/Professors.py
+++ Models/Professors.py
@@ -11,5 +11,14 @@ class professor(Base):
     last_name = Column(String)
     professor_key = Column(Integer, primary_key=True, autoincrement=True)
 
+    def to_dict(self):
+        return {
+            'nugget': self.nugget,
+            'first_name': self.first_name,
+            'middle_name': self.middle_name,
+            'last_name': self.last_name,
+            'profesorr_key': self.professor_key
+        }
+
     def __repr__(self):
         return str(self.first_name) + str(self.middle_name) + str(self.last_name)
